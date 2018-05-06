61e533c15386d9792a5ff0778d50d909f9dc1f0f
unknown <deverpham@gmail.com>
2018-05-06 08:43:29 -0700

    beautify stringurl ;

--- backend/models/db.py
+++ backend/models/db.py
@@ -1,15 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 POSTGRES = {
-    'user': 'postgres',
-    'pw': '',
+    'user': 'deverpham',
+    'pw': 'xxxxxxxxx',
     'db': 'clarifai',
-    'host': 'localhost',
+    'host': '192.168.1.101',
     'port': '5432',
 }
    
 def initDB(app):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-    %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+    print(app.config['SQLALCHEMY_DATABASE_URI'])
     db = SQLAlchemy(app) 
     return db
