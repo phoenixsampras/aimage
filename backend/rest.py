f118b815628a3b23c2df6e7296e4953154cf0842
unknown <deverpham@gmail.com>
2018-05-15 10:09:06 -0700

    final of final demo

--- backend/rest.py
+++ backend/rest.py
@@ -112,7 +112,7 @@ def checkIsFirst(array_search):
     isFirst = True;
     imageData = {};
     for search_result in array_search:
-        if search_result['score'] > 0.9:
+        if search_result['score'] > 0.8:
             isFirst = False
             imageData = search_result
             break
