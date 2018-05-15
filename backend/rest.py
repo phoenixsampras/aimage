e3d0543b6d9918222ca9d8316a7211151cdf687f
unknown <deverpham@gmail.com>
2018-05-14 19:30:06 -0700

    yes

--- backend/rest.py
+++ backend/rest.py
@@ -69,6 +69,7 @@ def addRecordToDatabase(path, arr_search):
         db.session.refresh(image)
     isFirst, imageData = checkIsFirst(arr_search)
     if(not isFirst):
+        print('not first')
         parentRoot = PictureVariants.query.filter(PictureVariants.input_id == imageData['input_id'], PictureVariants.order <=4,PictureVariants.is_remove == False);
         maxScore = -1;
         maxId = -1;
@@ -77,7 +78,7 @@ def addRecordToDatabase(path, arr_search):
             if( score > maxScore):
                 maxId = parent.picture_id
                 maxScore = score
-        list_search = PictureVariants.query.filter_by(picture_id = maxId).order_by(PictureVariants.order).limit(5)
+        list_search = PictureVariants.query.filter_by(picture_id = maxId, is_remove=False).order_by(PictureVariants.order).limit(5)
         arr_search = PictureVariants.serialize_list(list_search) + arr_search;
     for index,search in enumerate(arr_search):
         variant = PictureVariants.query.filter_by(url = search['url'], input_id = search['input_id'], picture_id= image.id).first()
@@ -85,6 +86,7 @@ def addRecordToDatabase(path, arr_search):
             variant = PictureVariants(picture_id=image.id, url= search['url'], score= search['score'], original_order=index, order = index, input_id= search['input_id'],metafilename = search['metafilename'], metaid = search['metaid'])
             db.session.add(variant)
             db.session.commit()
+
     return image.id;
 
 def getResult(imgid):
