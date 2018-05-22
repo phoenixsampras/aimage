da5dea4fb035fc0a86c4614075b7d7fbbfe95b49
unknown <deverpham@gmail.com>
2018-05-21 19:32:41 -0700

    fix ubuntu path

--- backend/vendor/module/load_router.py
+++ backend/vendor/module/load_router.py
@@ -5,5 +5,6 @@ for root, dirs, files in os.walk("router"):
     for file in files:
         if(file.endswith('.py')):
             modulePath = os.path.join(root, os.path.splitext(file)[0]).replace('\\', '.')
+            modulePath = os.path.join(root, os.path.splitext(file)[0]).replace('\/', '.')
             app.logger.info('loading router: '+ modulePath)
             importlib.import_module(modulePath)
